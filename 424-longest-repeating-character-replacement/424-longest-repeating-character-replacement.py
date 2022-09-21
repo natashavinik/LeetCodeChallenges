e55b8509923015a9b699cbfa4f4f87a1c4dfda12
Natasha Vinik <99206257+natashavinik@users.noreply.github.com>
2022-09-21 11:19:37 -0700

    Time: 420 ms (14.76%), Space: 13.9 MB (93.35%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -0,0 +1,40 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 1
+        l_dict = {}
+        left = 0
+        
+        for right in range(len(s)):
+            l_dict[s[right]] = l_dict.get(s[right], 0) + 1
+            while len(l_dict.items()) > k + 1:
+                if l_dict[s[left]] > 1:
+                    l_dict[s[left]] -= 1
+                else:
+                    l_dict.pop(s[left])
+                left += 1
+                
+            if (len(l_dict.items()) == 2 and min(l_dict.values()) <= k) or (len(l_dict.items()) == 1) or (sum(l_dict.values()) - max(l_dict.values()) <= k ):
+                res = max(res, right - left + 1)
+            else:
+                if l_dict[s[left]] > 1:
+                    l_dict[s[left]] -= 1
+                else:
+                    l_dict.pop(s[left])
+                left += 1
+        return res
+            
+                
+                
+                
+                
+                
+        #     if s[right] in l_dict:
+        #         l_dict[s[right]] += 1
+        #         res = max(res, l_dict[s[right]])
+        #     else:
+        #         l_dict = {}
+        #         l_dict[s[right]] = 1
+        # return res
+                
+            
+            
