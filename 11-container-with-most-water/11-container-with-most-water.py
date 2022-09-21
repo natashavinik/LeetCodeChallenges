db57a394fc737b184dc332053d650c587e2dbf3d
Natasha Vinik <99206257+natashavinik@users.noreply.github.com>
2022-09-21 10:39:57 -0700

    Time: 1920 ms (9.12%), Space: 27.5 MB (45.41%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -25,15 +25,8 @@ class Solution:
                 most_w = area
             if height[left] < height[right]:
                 left += 1
-            elif height[right] < height[left]:
-                right -= 1
             else:
-                if height[left + 1] > height[right -1]:
-                    left += 1
-                elif height[left + 1] < height[right -1]:
-                    right -= 1
-                else:
-                    left += 1
+                right -= 1
                 
         return most_w
             
