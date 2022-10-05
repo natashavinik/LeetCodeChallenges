f670da282b856db38a449b981aaab9ce8616ee7c
Natasha Vinik <99206257+natashavinik@users.noreply.github.com>
2022-10-05 07:36:56 -0700

    Time: 2362 ms (6.24%), Space: 22.8 MB (11.95%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,33 +1,29 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        l = 0
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+#         stack = [1]
+#         prices = [7,1,5,3,6,4]
+#         num = 5
+        most = 0
+        stack = []
+        # most = 4
+        for num in prices:
+            # print(num, stack)
+            while stack and num < stack[-1]:
+                stack.pop()
+            if stack != []:
+                most = max(most, num - stack[-1])
+            # while stack!= [] and num > stack[-1]:
+            #     stack.pop()
+            if stack == []:
+                stack.append(num)
         
-        for right in range(l, len(prices)):
-            if prices[right] < prices[l]:
-                l = right
-            profit = max(profit, prices[right] - prices[l])
-        return profit
-            
-#         if prices == sorted(prices):
-#             return (prices[-1] - prices[0])
-#         s_prices = sorted(prices)
-#         if prices == s_prices[::-1]:
-#             return 0
-        
-        
-#         profit = 0
-        
-#         for i in range(len(prices) - 1):
-#             if i != 0 and prices[i] <= prices[i - 1]:
-#                 continue
-#             rest = prices[i+1:]
-#             print(i, rest)
-#             m_rest = max(rest)
-#             if prices[i] >= max(rest):
-#                 continue
-#             if prices[i] < m_rest:
-#                 profit = max(profit, max(prices[i+1:]) - prices[i])
-#         return profit
+            elif num < stack[-1]:
+                stack.append(num)
+            # print("end", num, stack, most)
+        return most
                 
         
