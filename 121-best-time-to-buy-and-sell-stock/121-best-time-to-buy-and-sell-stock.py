f7ace647750808a2acf24e98f6a288b96b586785
Natasha Vinik <99206257+natashavinik@users.noreply.github.com>
2022-10-05 07:39:38 -0700

    Time: 1962 ms (24.91%), Space: 22.3 MB (95.18%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -7,23 +7,26 @@ class Solution(object):
 #         stack = [1]
 #         prices = [7,1,5,3,6,4]
 #         num = 5
-        most = 0
-        stack = []
-        # most = 4
-        for num in prices:
-            # print(num, stack)
-            while stack and num < stack[-1]:
-                stack.pop()
-            if stack != []:
-                most = max(most, num - stack[-1])
-            # while stack!= [] and num > stack[-1]:
-            #     stack.pop()
-            if stack == []:
-                stack.append(num)
+#         most = 0
+#         stack = []
+#         for num in prices:
+#             while stack and num < stack[-1]:
+#                 stack.pop()
+#             if stack != []:
+#                 most = max(most, num - stack[-1])
+#             if stack == []:
+#                 stack.append(num)
         
-            elif num < stack[-1]:
-                stack.append(num)
-            # print("end", num, stack, most)
-        return most
+#             elif num < stack[-1]:
+#                 stack.append(num)
+#         return most
+        profit = 0
+        l = 0
+        
+        for right in range(l, len(prices)):
+            if prices[right] < prices[l]:
+                l = right
+            profit = max(profit, prices[right] - prices[l])
+        return profit
                 
         
