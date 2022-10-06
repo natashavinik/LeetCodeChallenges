7d4a744a378474769a9bf33dc9841f7b3e692967
Natasha Vinik <99206257+natashavinik@users.noreply.github.com>
2022-10-06 08:42:53 -0700

    Time: 199 ms (13.60%), Space: 17.5 MB (15.06%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -4,45 +4,18 @@ class Solution:
             return [newInterval] 
         p_start, p_end = intervals[0][0], intervals[0][1]
         n_start, n_end = newInterval[0], newInterval[1]
-        print(n_start, n_end)
         overlap = []
-        resint = ["X", "X"]
-        i = 0
         for i in range(len(intervals)):
-            print("i", i)
             start = intervals[i][0]
             end = intervals[i][1]
             if n_start > end:
                 continue
-            # if n_start <= end and n_end <= end:
-            #     print("thisreturn", n_start, n_end, start, end)
-                # return intervals
             if start<= n_start <= end or start<= n_end <= end or n_start <= start <= end <= n_end:
                 overlap.append(i)
         if overlap == []:
             intervals = sorted(intervals + [newInterval])
             return intervals
         resint = [min(n_start, intervals[overlap[0]][0]), max(n_end, intervals[overlap[-1]][1])]
-        print("overlap", overlap)
-        print("resint", resint)
-#                 resint[0] = start
-#                 if end > n_end:
-#                     i += 1
-#                 else:
-#                     while end <= n_end:
-#                         overlap.append(i)
-#                         resint[1] = n_end
-#                         i += 1
-#                         start = intervals[i][0]
-#                         end = intervals[i][1]
-#                 print(resint)
-#             else:
-#                 i += 1
-                
-        print(overlap)
-        # if len(overlap) == 1:
-        #     intervals[overlap[0]] = resint
-        #     return intervals
         intervals = intervals[:overlap[0]] + [resint] + intervals[overlap[-1] + 1:]
         return intervals
         
