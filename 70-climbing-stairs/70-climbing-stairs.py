8c421456d8dfb3cb7ac113bcc4fa3efc2edd60ee
Natasha Vinik <99206257+natashavinik@users.noreply.github.com>
2022-10-05 20:42:57 -0700

    Time: 39 ms (77.58%), Space: 13.9 MB (11.73%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -15,20 +15,22 @@ class Solution:
         # for x in range(n-1):
         #     num1, num2 = num2, num1 + num2
         # return num2
-        if n <= 0:
-            return 0
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
+        # if n <= 0:
+        #     return 0
+        # if n == 1:
+        #     return 1
+        # if n == 2:
+        #     return 2
+        
+        return int((5**.5 / 5) * (((1 + 5**.5)/2)**(n + 1) - ((1 - 5**.5)/2)**(n + 1)))
 
-        results = [0] * (n + 1)
-        results[0:3] = [0, 1, 2]
+#         results = [0] * (n + 1)
+#         results[0:3] = [0, 1, 2]
 
-        for i in range(3, n + 1):
-            results[i] = results[i - 1] + results[i - 2]
+#         for i in range(3, n + 1):
+#             results[i] = results[i - 1] + results[i - 2]
 
-        return results[n]
+#         return results[n]
 #         2 => 2:
 #             1 + 1
 #             2
