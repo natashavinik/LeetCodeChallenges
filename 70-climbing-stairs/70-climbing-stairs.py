a21dcd108847e076696adca10872ee9158b45885
Natasha Vinik <99206257+natashavinik@users.noreply.github.com>
2022-10-07 11:20:25 -0700

    Time: 52 ms (48.28%), Space: 13.8 MB (56.93%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,60 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # ways = 0 2
-#         last = [1, 2]
-#         if n == 1:
-#             return 1
-#         if n == 2:
-#             return 2
-        
-#         for num in range(3, n + 1):
-#             n_way = last[-1] + last[-2]
-#             last.append(n_way)
-#         return last[-1]
-        # num1 = num2 = 1
-        # for x in range(n-1):
-        #     num1, num2 = num2, num1 + num2
-        # return num2
-        # if n <= 0:
-        #     return 0
-        # if n == 1:
-        #     return 1
-        # if n == 2:
-        #     return 2
-        
-        return int((5**.5 / 5) * (((1 + 5**.5)/2)**(n + 1) - ((1 - 5**.5)/2)**(n + 1)))
-
-#         results = [0] * (n + 1)
-#         results[0:3] = [0, 1, 2]
-
-#         for i in range(3, n + 1):
-#             results[i] = results[i - 1] + results[i - 2]
-
-#         return results[n]
-#         2 => 2:
-#             1 + 1
-#             2
-        
-#         3 => 3:
-#             1 + 1 + 1
-#             2 + 1
-#             1 + 2
+        stack = [0, 1, 2]
+        if n < 3:
+            return(stack[n])
+        for i in range(3, n+1):
             
-#         4 => 5: 
-#             1 + 1 + 1 + 1
-#             1 + 1 + 2
-#             1 + 2 + 1
-#             2 + 1 + 1
-#             2 + 2
-       
-#     5 => 8:
-            
-#             1 + 1 + 1 + 1 + 1
-#             1 + 1 + 2 + 1
-#             1 + 2 + 1 + 1
-#             2 + 1 + 1 + 1
-#             1 + 1 + 1 + 2
-#             2 + 2 + 1
-#             2 + 1 + 2
-#             1 + 2 + 2
-        
+            stack.append(stack[-1] + stack[-2])
+        return stack[-1]
