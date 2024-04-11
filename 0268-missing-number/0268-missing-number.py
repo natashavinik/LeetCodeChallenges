afabff1b6f5bf304c08fa0e62646ca8c5574d833
Natasha Vinik <99206257+natashavinik@users.noreply.github.com>
2024-04-11 16:14:13 -0700

    Time: 96 ms (60.73%), Space: 13.6 MB (7.57%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,12 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
+        num_set = set(nums)
+        n = len(nums) + 1
         
         for i in range(n):
-            print(i)
-            if i in nums:
-                continue
-            else:
+            if i not in num_set:
                 return i
-        return n
+#         n = len(nums)
+        
+#         for i in range(n):
+#             print(i)
+#             if i in nums:
+#                 continue
+#             else:
+#                 return i
+#         return n
+    
